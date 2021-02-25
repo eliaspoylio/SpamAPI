8e5b92b3d5ad5b5cb661bc15b0b59c3389422138
Elias Pöyliö <elias.poylio@gmail.com>
2021-02-25 21:43:51 +0200

    initial commit

---
+++ SpamAPI/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpamAPI.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string MessageFrom { get; set; }
+        public string MessageAddress { get; set; }
+        public string MessageDate { get; set; }
+        public string MessageContent { get; set; }
+    }
+}
---
+++ SpamAPI/Models/SpamDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpamAPI.Models
+{
+    public class SpamDbContext : DbContext
+    {
+        public DbSet<Message> Message { get; set; }
+
+        public SpamDbContext(DbContextOptions<SpamDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Message>().ToTable("Message");
+        }
+    }
+}
